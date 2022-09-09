3bc5fffbb59c38615d68a981c546868035a68eaf
Tona <tona@mango-byte.com>
2022-09-09 10:48:17 +0700

    f

--- simple-persistence/src/main/java/com/mb/persistence/config/PersistenceConfig.java
+++ simple-persistence/src/main/java/com/mb/persistence/config/PersistenceConfig.java
@@ -1,13 +1,11 @@
 package com.mb.persistence.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories("com.mb.persistence.repositories")
-@EntityScan("com.mb.persistence.models")
+@EnableJpaRepositories()
 public class PersistenceConfig {
 }

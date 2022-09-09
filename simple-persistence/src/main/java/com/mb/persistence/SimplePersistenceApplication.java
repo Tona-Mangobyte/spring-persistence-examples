fc1976c363e58f94a83b8b54f049e5aac23be222
Tona <tona@mango-byte.com>
2022-09-09 09:47:40 +0700

    init simple-persistence/module

---
+++ simple-persistence/src/main/java/com/mb/persistence/SimplePersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.mb.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePersistenceApplication.class, args);
+	}
+
+}
---
+++ simple-persistence/src/test/java/com/mb/persistence/SimplePersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
